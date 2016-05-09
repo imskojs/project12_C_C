796eae4c410631077b913310a5decf40ed3842c8
Seunghoon Ko <imskojs@gmail.com>
2016-05-09 18:31:41 +0900

    walkthrough

--- codes/state/Walkthrough/WalkthroughController.js
+++ codes/state/Walkthrough/WalkthroughController.js
@@ -4,14 +4,29 @@
     .controller('WalkthroughController', WalkthroughController);
 
   WalkthroughController.$inject = [
+    '$ionicSlideBoxDelegate', '$ionicViewSwitcher', '$state',
     'WalkthroughModel'
   ];
 
   function WalkthroughController(
+    $ionicSlideBoxDelegate, $ionicViewSwitcher, $state,
     WalkthroughModel
   ) {
     var vm = this;
     vm.Model = WalkthroughModel;
+
+    vm.next = next;
+
+    //====================================================
+    //  VM
+    //====================================================
+    function next(login) {
+      if (login) {
+        $ionicViewSwitcher.nextDirection('forward');
+        return $state.go('Login');
+      }
+      return $ionicSlideBoxDelegate.next();
+    }
 
   }
 })();
