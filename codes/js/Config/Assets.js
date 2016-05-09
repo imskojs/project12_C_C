30135427123ef8a03e410bc1503ebf636e98f95f
Seunghoon Ko <imskojs@gmail.com>
2016-05-09 16:09:19 +0900

    install dependencies, fix bugs

--- codes/js/Config/Assets.js
+++ codes/js/Config/Assets.js
@@ -3,6 +3,6 @@
   'use strict';
 
   angular.module('app')
-    .value('Assets', []);
+    .value('Assets', ["img/btn_delete.png","img/category_01.png","img/category_02.png","img/category_03.png","img/category_04.png","img/category_05.png","img/category_06.png","img/category_07.png","img/category_08.png","img/category_09.png","img/category_10.png","img/category_11png.png","img/category_12png.png","img/category_13png.png","img/category_14png.png","img/category_15.png","img/category_16.png","img/category_17.png","img/category_18.png","img/category_icon_01.png","img/category_icon_02.png","img/category_icon_03.png","img/category_icon_04.png","img/category_icon_05.png","img/category_icon_06.png","img/category_icon_07.png","img/category_icon_08.png","img/category_icon_09.png","img/category_icon_10.png","img/category_icon_11.png","img/category_icon_12.png","img/category_icon_13.png","img/category_icon_14.png","img/category_icon_15.png","img/category_icon_16.png","img/category_icon_17.png","img/category_icon_18.png","img/category_unselect.png","img/ic_back.png","img/ic_drop_arrow.png","img/ic_search.png","img/login_bg.png","img/login_ic_01.png","img/main02_15.png","img/main_ic_01.png","img/main_ic_02.png","img/make_survey002_03.png","img/nav_ic_01.png","img/nav_ic_02.png","img/nav_ic_03.png","img/nav_ic_04.png","img/nav_ic_05.png","img/nav_ic_06.png","img/nav_ic_07.png","img/nav_ic_08.png","img/signin_ic_01.png","img/write_btn_01.png","img/write_ic_01.png","img/wrote_ic_01_07.png","img/wrote_ic_01_11.png"]);
 
 })(angular);
